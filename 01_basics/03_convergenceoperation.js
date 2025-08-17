415550e7d51c8322829c49024bcaae2af4962f2a
Soham Kale <150458772+Soham1500@users.noreply.github.com>
2025-08-17 14:52:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_convergenceoperation.js
@@ -0,0 +1,26 @@
+let score = 33
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=> 33
+//"33abc"=> NaN but type is number 
+// true => 1 ; false =>0 
+
+let isLoggedIn = 0
+let boolisloggedin = Boolean(isLoggedIn);
+console.log(boolisloggedin);
+
+
+// when converting boolean 1=> true and 0 =>false
+// ""=>false 
+// "hitesh"=>true
+
+let age = 22
+let convertedage = String(age)
+console.log(convertedage);
+console.log(typeof convertedage);
+
+
